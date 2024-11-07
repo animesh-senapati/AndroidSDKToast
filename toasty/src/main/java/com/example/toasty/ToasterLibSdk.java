a43c4d3fa8386b99501abfb8b59a5a8f0f51be14
animesh-senapati <asenapati229@gmail.com>
2024-11-07 16:26:59 +0530

    Code Pushed

---
+++ toasty/src/main/java/com/example/toasty/ToasterLibSdk.java
@@ -0,0 +1,12 @@
+package com.example.toasty;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToasterLibSdk {
+
+    public static void simpleToast(Context context,String msg){
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+
+    }
+}
